7ba5fa9d78157b2bb8e6b82cf673e70aa9277379
duythnh <duytle02@gmail.com>
2024-09-28 18:22:45 +0700

    passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (word, numRepeat) {
+  result = "";
+  if (numRepeat < 0) return "ERROR";
+  for (let i = 0; i < numRepeat; i++) {
+    result += word;
+  }
+  return result;
 };
 
 // Do not edit below this line
