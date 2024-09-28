3ae6b606d28bd5b8a97ab9b6c829fa98e6f4fc0f
duythnh <duytle02@gmail.com>
2024-09-28 19:28:06 +0700

    passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  const newTemperature = (5 / 9) * (temperature - 32);
+  if (Number.isInteger(newTemperature)) return newTemperature;
+  return +newTemperature.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  const newTemperature = (9 / 5) * temperature + 32;
+  if (Number.isInteger(newTemperature)) return newTemperature;
+  return +newTemperature.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
