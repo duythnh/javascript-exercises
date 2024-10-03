23133c1d4eca36dc7fc6f150ebc2601cdedb5fb3
duythnh <duytle02@gmail.com>
2024-10-03 19:30:09 +0700

    passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count;
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
 
+    if (count < 0) return 'OOPS';
+    if (count === 0) return 0;
+
+    let first = 1
+    let second = 0
+    for (let i = 2; i <= count; i++) {
+        let curr = first + second
+        second = first
+        first = curr
+    }
+    return first
 };
 
 // Do not edit below this line
