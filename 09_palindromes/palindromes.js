7863bd8a023c6a29491cab8d7c040b3ea134d898
duythnh <duytle02@gmail.com>
2024-10-03 19:21:18 +0700

    passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const cleanedStr = str
+    .toLowerCase()
+    .split('')
+    .filter((char) => alphanumerical.includes(char))
+    .join("");
 
+  const reverseStr = cleanedStr.split("").reverse().join("");
+  return reverseStr === cleanedStr;
 };
 
 // Do not edit below this line
